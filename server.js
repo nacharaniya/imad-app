fdaa4d408906170fada96340960154a66fa5aabc
Junaid447 <30664150+Junaid447@users.noreply.github.com>
2017-08-11 14:51:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
-<html>
-    <head>
-        
-        <title>
-            Article|one
-        </title>
-    </head>
-    <body>
-        <div>
-            home
-        </div>
-        <h2>
-            Article one
-        </h2>
-        <div>
-            Aug 11 2017
-        </div>
-        <div>
-        <p>
-            This is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article one
-        </p>
-        <p>
-            This is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article one
-        </p>
-        <p>
-            This is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article oneThis is article one
-        </p>
-        </div>
-    </body>
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-    </html>
-    
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/article-one',function(req,res)
+{
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));  
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
